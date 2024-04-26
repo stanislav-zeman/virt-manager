933527c3a0e28f5d0c7cecbe0f07705137307001
Stanislav Zeman <zeman@standa.dev>
2024-04-26 18:51:00 +0200

    style(app): update app struct comments to follow go doc

--- internal/app/app.go
+++ internal/app/app.go
@@ -5,23 +5,21 @@ import (
 	"fmt"
 )
 
-// App struct
 type App struct {
 	ctx context.Context
 }
 
-// NewApp creates a new App application struct
 func New() *App {
 	return &App{}
 }
 
-// startup is called when the app starts. The context is saved
-// so we can call the runtime methods
+// Startup is called when the app starts.
+// The context is saved so we can call the runtime methods.
 func (a *App) Startup(ctx context.Context) {
 	a.ctx = ctx
 }
 
-// Greet returns a greeting for the given name
+// Greet returns a greeting for the given name.
 func (a *App) Greet(name string) string {
 	return fmt.Sprintf("Hello %s, It's show time!", name)
 }
