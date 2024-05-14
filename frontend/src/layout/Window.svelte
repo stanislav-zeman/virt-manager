ebf2a0c21cac4c7904a904a6b7086d23842416d6
Stanislav Zeman <zeman@standa.dev>
2024-05-14 19:33:45 +0200

    feat(fronend): setup settings tab

--- frontend/src/layout/Window.svelte
+++ frontend/src/layout/Window.svelte
@@ -5,6 +5,7 @@
 </script>
 
 <div class="w-4/5">
-    <Heading tag="h2" class="my-6 text-white">{heading}</Heading>
+    <Heading tag="h3" class="my-6 text-white">{heading}</Heading>
+    <hr class="w-3/5 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-6" />
     <slot />
 </div>
--- frontend/src/routes/Settings.svelte
+++ frontend/src/routes/Settings.svelte
@@ -1,5 +1,21 @@
 <script>
     import Window from "../layout/Window.svelte";
+    import { Label, Input, InputAddon, ButtonGroup } from "flowbite-svelte";
+    import { ServerOutline } from "flowbite-svelte-icons";
 </script>
 
-<Window heading="Settings" />
+<Window heading="Settings">
+    <div class="flex-col">
+        <Label for="hypervisor-uri" class="block text-white mb-2"
+            >Hypervisor URI</Label
+        >
+        <ButtonGroup class="w-2/5">
+            <InputAddon>
+                <ServerOutline
+                    class="w-4 h-4 text-gray-500 dark:text-gray-400"
+                />
+            </InputAddon>
+            <Input id="hypervisor-uri" placeholder="quemu:///system" />
+        </ButtonGroup>
+    </div>
+</Window>
