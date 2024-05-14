9debd70ebf59c2b0485a45f85d8abdbd7f5e0030
Stanislav Zeman <zeman@standa.dev>
2024-05-14 22:15:47 +0200

    feat(frontend): rework domains route mounting

--- frontend/src/routes/Domains.svelte
+++ frontend/src/routes/Domains.svelte
@@ -1,36 +1,48 @@
 <script lang="ts">
+    import { onMount } from "svelte";
     import Window from "../layout/Window.svelte";
     import { Domains, Connected } from "../../wailsjs/go/app/App";
-    import { Button, P, Spinner } from "flowbite-svelte";
+    import { domain } from "../../wailsjs/go/models";
+    import { Button, P } from "flowbite-svelte";
     import Domain from "../components/Domain.svelte";
     import { getNotificationsContext } from "svelte-notifications";
 
     const { addNotification } = getNotificationsContext();
+
+    let connected: boolean;
+    let domains: domain.Domain[] = [];
+    async function reloadDomains() {
+        try {
+            domains = await Domains();
+        } catch (error) {
+            addNotification({
+                text: "Failed retrieving domain data",
+                position: "bottom-right",
+                type: "error",
+                removeAfter: 5000,
+            });
+        }
+    }
+
+    onMount(async () => {
+        connected = await Connected();
+        if (connected) {
+            reloadDomains();
+        }
+    });
 </script>
 
 <Window heading="Domains">
-    {#await Connected()}
-        <Spinner class="m-6" />
-    {:then state}
-        {#if state}
-            <div>
-                {#await Domains()}
-                    <Spinner class="m-6" />
-                {:then dss}
-                    {#each dss as d}
-                        <Domain name={d.name} status={d.status} />
-                    {/each}
-                {:catch error}
-                    An error occured: {error}
-                {/await}
-            </div>
-            <div class="justify-start">
-                <Button on:click{domains()}>Refresh</Button>
-            </div>
-        {:else}
-            <P class="text-center text-white" size="xl"
-                >You are not connected!</P
-            >
-        {/if}
-    {/await}
+    {#if connected}
+        <div>
+            {#each domains as d}
+                <Domain name={d.name} status={d.status} />
+            {/each}
+        </div>
+        <div class="justify-start">
+            <Button on:click{reloadDomains}>Refresh</Button>
+        </div>
+    {:else}
+        <P class="text-center text-white" size="xl">You are not connected!</P>
+    {/if}
 </Window>
